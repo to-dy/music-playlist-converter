ab2bb43acf1a4c798ee72559f1cb5cf723e59489
Tolu Yussuff <yussufftolu@gmail.com>
2023-05-30 00:01:07 +0100

    api error types

---
+++ api/handlers/error.go
@@ -0,0 +1,20 @@
+package handlers
+
+type ErrorObject struct {
+	Status int          `json:"status,omitempty"`
+	Title  string       `json:"title,omitempty"`
+	Detail string       `json:"detail,omitempty"`
+	Source *ErrorSource `json:"source,omitempty"`
+	Meta   interface{}  `json:"meta,omitempty"`
+}
+
+type ErrorSource struct {
+	Pointer   string `json:"pointer,omitempty"`
+	Parameter string `json:"parameter,omitempty"`
+}
+
+type Errors []*ErrorObject
+
+type ErrorResponse struct {
+	Errors Errors `json:"errors"`
+}
